794fe548ca09ce7a786c819a4c496056c64b1c99
Yael20026 <hiram060220@gmail.com>
2025-03-27 13:27:40 -0600

    ya modificamos muchas cosas

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import requests
+import json
+import os
 
 # Token de Mapbox (reemplázalo por tu propio token)
 MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiaGlyYW0wNjAyMjAiLCJhIjoiY204cGx2MG53MGM2eDJqb21ud2h1enIwOCJ9.nd8HgkDj3SEJlr-oQY8ufg'
@@ -21,10 +23,32 @@ def home():
 def rutas():
     return render_template('rutas.html')
 
-
+@app.route('/reportes')
+def resportes():
+    return render_template('reportes.html')
 @app.route('/recargas')
-def recargas():
-    return render_template('recargas.html')
+def puntos_recarga():
+    file_path = r'C:\Users\XOCEA\OneDrive\Documentos\YOVOY\app\static\puntosRecarga.json'
+    if not os.path.exists(file_path):
+        return jsonify({"error": "Archivo puntosRecarga.json no encontrado"}), 404
+    
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    
+    return render_template('recargas.html', puntos=data)
+
+@app.route('/api/puntos_recarga')
+def api_puntos_recarga():
+    file_path = os.path.join('static', 'puntosRecarga.json')
+    file_path=r"C:\Users\XOCEA\OneDrive\Documentos\YOVOY\app\static\puntosRecarga.json"
+    if not os.path.exists(file_path):
+        return jsonify({"error": "Archivo puntosRecarga.json no encontrado"}), 404
+
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    
+    return jsonify(data)
+
 
 # Ruta para la página de búsqueda de rutas
 @app.route('/buscar_rutas', methods=['GET'])
